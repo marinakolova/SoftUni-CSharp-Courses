5400d1218d0b95ebdd4022754d886e632183699b
marinakolova <marina.kolova.359@gmail.com>
2021-01-03 15:06:58 +0200

    added solved exam

---
+++ Algorithms-Fundamentals-with-CSharp-November-2020/Exam-03-Jan-2021/02-Socks/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_Socks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var leftSocks = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var rightSocks = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var table = new int[leftSocks.Length + 1, rightSocks.Length + 1];
+
+            for (int r = 1; r < table.GetLength(0); r++)
+            {
+                for (int c = 1; c < table.GetLength(1); c++)
+                {
+                    if (leftSocks[r - 1] == rightSocks[c - 1])
+                    {
+                        table[r, c] = table[r - 1, c - 1] + 1;
+                    }
+                    else
+                    {
+                        table[r, c] = Math.Max(table[r, c - 1], table[r - 1, c]);
+                    }
+                }
+            }
+
+            var row = leftSocks.Length;
+            var col = rightSocks.Length;
+
+            var lcsCount = 0;
+
+            while (row > 0 && col > 0)
+            {
+                if (leftSocks[row - 1] == rightSocks[col - 1])
+                {
+                    lcsCount += 1;
+                    row -= 1;
+                    col -= 1;
+                }
+                else if (table[row, col - 1] >= table[row - 1, col])
+                {
+                    col -= 1;
+                }
+                else
+                {
+                    row -= 1;
+                }
+            }
+
+            Console.WriteLine(lcsCount);
+        }
+    }
+}
---
+++ Algorithms-Fundamentals-with-CSharp-November-2020/Exam-03-Jan-2021/03-PathFinder/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_PathFinder
+{
+    class Program
+    {
+        private static Dictionary<int, List<int>> graph;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());            
+            graph = ReadGraph(n);
+
+            var p = int.Parse(Console.ReadLine());
+            for (int i = 0; i < p; i++)
+            {
+                var path = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (CheckPath(path))
+                {
+                    Console.WriteLine("yes");
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+            }
+        }
+
+        private static bool CheckPath(int[] path)
+        {
+            for (int i = 0; i < path.Length - 1; i++)
+            {
+                if (!HasDirectPath(path[i], path[i + 1]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool HasDirectPath(int source, int destination)
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(source);
+            var visited = new HashSet<int> { source };
+            
+            foreach (var child in graph[source])
+            {
+                if (visited.Contains(child))
+                {
+                    continue;
+                }
+
+                visited.Add(child);
+                queue.Enqueue(child);
+            }            
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node == destination)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static Dictionary<int, List<int>> ReadGraph(int n)
+        {
+            var result = new Dictionary<int, List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                result.Add(i, new List<int>());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                result[i].AddRange(Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList());
+            }
+            return result;
+        }
+    }
+}
