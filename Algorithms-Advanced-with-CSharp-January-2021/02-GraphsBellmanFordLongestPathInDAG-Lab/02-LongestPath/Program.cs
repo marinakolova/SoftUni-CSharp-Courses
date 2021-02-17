e36981eb41e9aa398cf7b3a67a80f308a389cf36
marinakolova <marina.kolova.359@gmail.com>
2021-02-18 00:35:42 +0200

    added solved tasks

---
+++ Algorithms-Advanced-with-CSharp-January-2021/02-GraphsBellmanFordLongestPathInDAG-Lab/02-LongestPath/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_LongestPath
+{
+    public class Edge
+    {
+        public int From { get; set; }
+
+        public int To { get; set; }
+
+        public int Weight { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.From} {this.To} {this.Weight}";
+        }
+    }
+
+    public class Program
+    {
+        private static List<Edge>[] graph;
+
+        public static void Main(string[] args)
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edgesCount = int.Parse(Console.ReadLine());
+
+            graph = new List<Edge>[nodes + 1];
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                graph[node] = new List<Edge>();
+            }
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var from = edgeData[0];
+                var to = edgeData[1];
+                var weight = edgeData[2];
+
+                graph[from].Add(new Edge
+                {
+                    From = from,
+                    To = to,
+                    Weight = weight
+                });
+            }
+
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+            
+            var sortedNodes = TopologicalSorting();
+
+            var distances = new double[graph.Length];
+            Array.Fill(distances, double.NegativeInfinity);
+
+            distances[source] = 0;
+
+            while (sortedNodes.Count > 0)
+            {
+                var node = sortedNodes.Pop();
+
+                foreach (var edge in graph[node])
+                {
+                    var newDistance = distances[node] + edge.Weight;
+
+                    if (newDistance > distances[edge.To])
+                    {
+                        distances[edge.To] = newDistance;
+                    }
+                }
+            }
+
+            Console.WriteLine(distances[destination]);
+        }
+
+        private static Stack<int> TopologicalSorting()
+        {
+            var visited = new bool[graph.Length];
+            var sorted = new Stack<int>();
+
+            for (int node = 1; node < graph.Length; node++)
+            {
+                DFS(node, visited, sorted);
+            }
+
+            return sorted;
+        }
+
+        private static void DFS(int node, bool[] visited, Stack<int> sorted)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            foreach (var edge in graph[node])
+            {
+                DFS(edge.To, visited, sorted);
+            }
+
+            sorted.Push(node);
+        }
+    }
+}
