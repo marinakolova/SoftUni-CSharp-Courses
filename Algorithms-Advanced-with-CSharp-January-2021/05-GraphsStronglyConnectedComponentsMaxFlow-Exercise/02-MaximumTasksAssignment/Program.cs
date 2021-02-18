4897d125e00b57753b7f2b7971534f1eb8b6c07b
marinakolova <marina.kolova.359@gmail.com>
2021-02-18 22:56:26 +0200

    added solved tasks

---
+++ Algorithms-Advanced-with-CSharp-January-2021/05-GraphsStronglyConnectedComponentsMaxFlow-Exercise/02-MaximumTasksAssignment/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_MaximumTasksAssignment
+{
+    public class Program
+    {
+        private static int[,] graph;
+        private static int[] parents;
+        
+        public static void Main(string[] args)
+        {
+            var peopleCount = int.Parse(Console.ReadLine());
+            var tasksCount = int.Parse(Console.ReadLine());
+
+            // 0, 1, 2, 3, 4, 5, 6, 7
+            // s, A, B, C, 1, 2, 3, t
+
+            var nodes = peopleCount + tasksCount + 2;
+            var start = 0;
+            var target = nodes - 1;
+
+            graph = ConstructGraph(peopleCount, tasksCount, nodes, start, target);
+            
+            parents = new int[graph.GetLength(0)];
+            Array.Fill(parents, -1);
+            
+            while (BFS(start, target))
+            {
+                var node = target;
+                
+                while (node != start)
+                {
+                    var parent = parents[node];
+
+                    graph[parent, node] = 0;
+                    graph[node, parent] = 1;
+
+                    node = parent;
+                }
+            }
+
+            //for (int row = 0; row < graph.GetLength(0); row++)
+            //{
+            //    for (int col = 0; col < graph.GetLength(1); col++)
+            //    {
+            //        Console.Write($"{graph[row, col]} ");
+            //    }
+            //
+            //    Console.WriteLine();
+            //}
+
+            for (int person = 1; person <= peopleCount; person++)
+            {
+                for (int task = peopleCount + 1; task <= peopleCount + tasksCount; task++)
+                {
+                    if (graph[task, person] > 0)
+                    {
+                        Console.WriteLine($"{ (char)(64 + person) }-{task - peopleCount}");
+                    }
+                }
+            }
+        }
+
+        private static bool BFS(int start, int target)
+        {
+            var visited = new bool[graph.GetLength(0)];
+            var queue = new Queue<int>();
+
+            visited[start] = true;
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node == target)
+                {
+                    return true;
+                }
+
+                for (int child = 0; child < graph.GetLength(1); child++)
+                {
+                    if (!visited[child] &&
+                        graph[node, child] > 0)
+                    {
+                        parents[child] = node;
+                        visited[child] = true;
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static int[,] ConstructGraph(int peopleCount, int tasksCount, int nodes, int start, int target)
+        {
+            var result = new int[nodes, nodes];
+
+            for (int person = 1; person <= peopleCount; person++)
+            {
+                result[start, person] = 1;
+            }
+
+            for (int task = peopleCount + 1; task <= peopleCount + tasksCount; task++)
+            {
+                result[task, target] = 1;
+            }
+
+            for (int person = 1; person <= peopleCount; person++)
+            {
+                var personCapabilities = Console.ReadLine();
+
+                for (int task = 0; task < personCapabilities.Length; task++)
+                {
+                    if (personCapabilities[task] == 'Y')
+                    {
+                        result[person, peopleCount + 1 + task] = 1;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Algorithms-Advanced-with-CSharp-January-2021/05-GraphsStronglyConnectedComponentsMaxFlow-Exercise/03-FindBiConnectedComponents/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_FindBiConnectedComponents
+{
+    public class Program
+    {
+        private static List<int>[] graph;
+        private static int[] depths;
+        private static int[] lowpoint;        
+        private static bool[] visited;
+        private static int[] parent;
+
+        private static Stack<int> stack;
+        private static List<HashSet<int>> components;
+
+        public static void Main(string[] args)
+        {
+            var nodesCount = int.Parse(Console.ReadLine());
+            var edgesCount = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(nodesCount, edgesCount);
+
+            depths = new int[nodesCount];
+            lowpoint = new int[nodesCount];
+            visited = new bool[nodesCount];
+
+            parent = new int[nodesCount];
+            Array.Fill(parent, -1);
+
+            stack = new Stack<int>();
+            components = new List<HashSet<int>>();
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                if (!visited[node])
+                {
+                    FindArticulationPoint(node, 1);
+
+                    var component = new HashSet<int>();
+
+                    while (stack.Count > 1)
+                    {
+                        var stackChild = stack.Pop();
+                        var stackNode = stack.Pop();
+
+                        component.Add(stackNode);
+                        component.Add(stackChild);
+                    }
+
+                    components.Add(component);
+                }
+            }
+
+            Console.WriteLine($"Number of bi-connected components: {components.Count}");
+
+            //foreach (var component in components)
+            //{
+            //    Console.WriteLine(string.Join(" ", component));
+            //}
+        }
+
+        private static void FindArticulationPoint(int node, int depth)
+        {
+            visited[node] = true;            
+            depths[node] = depth;
+            lowpoint[node] = depth;
+
+            var childCount = 0;
+
+            foreach (var child in graph[node])
+            {
+                if (!visited[child])
+                {
+                    stack.Push(node);
+                    stack.Push(child);
+
+                    parent[child] = node;
+                    childCount += 1;
+                    
+                    FindArticulationPoint(child, depth + 1);
+
+                    if ((parent[node] == -1 && childCount > 1) ||
+                        (parent[node] != -1 && lowpoint[child] >= depth))
+                    {
+                        var component = new HashSet<int>();
+                        
+                        while (true)
+                        {
+                            var stackChild = stack.Pop();
+                            var stackNode = stack.Pop();
+
+                            component.Add(stackNode);
+                            component.Add(stackChild);
+
+                            if (stackNode == node &&
+                                stackChild == child)
+                            {
+                                break;
+                            }
+                        }
+
+                        components.Add(component);
+                    }
+
+                    lowpoint[node] = Math.Min(lowpoint[node], lowpoint[child]);
+                }
+                else if (parent[node] != child &&
+                    depths[child] < lowpoint[node])
+                {
+                    lowpoint[node] = depths[child];
+
+                    stack.Push(node);
+                    stack.Push(child);
+                }
+            }
+        }
+
+        private static List<int>[] ReadGraph(int nodesCount, int edgesCount)
+        {
+            var result = new List<int>[nodesCount];
+
+            for (int node = 0; node < result.Length; node++)
+            {
+                result[node] = new List<int>();
+            }
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                var edge = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var first = edge[0];
+                var second = edge[1];
+                
+                result[first].Add(second);
+                result[second].Add(first);
+            }
+
+            return result;
+        }
+    }
+}
